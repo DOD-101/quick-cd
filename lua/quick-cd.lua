b35a4185a3dc07ff8a01ccb4a488ac145b50e46d
David Thievon <pdkfan@gmail.com>
2025-12-09 07:56:38 +0100

    feat(setup): enable adding custom points

--- lua/quick-cd.lua
+++ lua/quick-cd.lua
@@ -9,14 +9,29 @@
 --- `{ "home", "user", ".config", "neovim"}`
 ---
 ---@alias Path string[]
+---
+
+--- Options passed to setup
+---
+--- @class SetupOpts
+--- @field points {[string]: Point} Additional points
 
 -- Main plugin code
 
 local plugin = {}
 
-function plugin.setup(_)
+---Setup function to load the plugin
+---@param opts SetupOpts
+function plugin.setup(opts)
 	plugin.utils = require("quick-cd.utils")
 	plugin.points = require("quick-cd.points")
+
+	if opts.points then
+		for k, v in pairs(opts.points) do
+			plugin.points[k] = v
+		end
+	end
+
 	require("quick-cd.cmd")
 end
 
